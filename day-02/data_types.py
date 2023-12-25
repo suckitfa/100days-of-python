802bb5586058ddc394fc4d27c88be0f0c7f80b9a
GuangyuanTang <254202042@qq.com>
2023-12-25 18:18:55 +0800

    feat: two digits num

---
+++ day-02/data_types.py
@@ -0,0 +1,26 @@
+# String
+print("hello"[0])
+
+# Integer
+print(1+2)
+new_var = 123_123_123
+print(new_var * 12)
+
+# Float
+print(3.123232)
+
+# Bolean
+print(False)
+print(True)
+
+name = input("Your name:")
+num_char = len(name)
+print(type(num_char))
+print("Your name has " + str(num_char) + " characters.")
+
+print(70 + float("100.2"))
+
+num = input("Input two digits number:")
+num_str = str(num)
+[fisrt_num,sec_num] = num_str
+print(str(int(fisrt_num) + int(sec_num))) 
