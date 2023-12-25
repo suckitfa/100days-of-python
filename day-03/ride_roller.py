cc53651ef663cc4ee50ea53adda274cff08099fb
GuangyuanTang <254202042@qq.com>
2023-12-25 23:21:03 +0800

    feat: make ticket payer better

--- day-03/ride_roller.py
+++ day-03/ride_roller.py
@@ -1,14 +1,22 @@
 print("wELCOME TO THE ROLLERCOASTER!")
 height = int(input("What is your height in cm?"))
-
+bill = 0
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age?"))
     if age < 12:
+        bill = 4
         print("Please pay $4.")
     elif age <= 18:
+        bill = 8
         print("Plean pay $8")
     else:
+        bill = 18
         print("Please pay $18.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
+
+wants_photo = input("Want photo taken?(Y or N): ")
+if wants_photo == 'Y':
+    bill += 3
+print(f"Total pay: {bill}")
