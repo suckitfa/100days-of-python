daaf22dcff369a7e256b5be7036f8567f425442a
GuangyuanTang <254202042@qq.com>
2023-12-25 18:46:05 +0800

    feat: left cal

---
+++ day-02/your_life_in_weeks.py
@@ -0,0 +1,6 @@
+age = int(input("What is your age?"))
+year = 90 - age
+months = year * 12
+weeks = year * 52
+days = year * 365
+print(f"You have {days} days left, {weeks} weeks left, and {months} months left.")
