15973c759b3d2e66955bad93a4f4fbf8ab079d90
GuangyuanTang <254202042@qq.com>
2023-12-27 21:20:47 +0800

    feat: better avg height cal

--- day-05/cal_avg_heights.py
+++ day-05/cal_avg_heights.py
@@ -3,9 +3,7 @@ for n in range(0,len(student_heights)):
     student_heights[n] = int(student_heights[n])
 print(student_heights)
 # 156 178 165 171 187
-total = 0
-
-for h in student_heights:
-    total += h
-avg_height = total / len(student_heights)
+total = sum(student_heights)
+num_of_student = len(student_heights)
+avg_height = total / num_of_student
 print(f"avg heights = {avg_height}")
