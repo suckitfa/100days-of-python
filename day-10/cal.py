b60517740590cab26cf0cbcbac199b1f58fccb3d
GuangyuanTang <254202042@qq.com>
2024-01-06 17:55:01 +0800

    feat: cal wit recursion

--- day-10/cal.py
+++ day-10/cal.py
@@ -29,18 +29,23 @@ operations = {
     "/":divide
 }
 
-num1 = float(input("What's the first number?:"))
-for symbol in operations:
-    print(symbol)
 
-should_continue = True
-while should_continue:
-    operation_symbol = input("Pick an operation from the line above:")
-    cal_func = operations[operation_symbol]
-    num2 = float(input("What's the next number?:"))
-    answer = cal_func(num1,num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    if input(f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation") == 'y':
-        num1 = answer
-    else:
-        should_continue = False
+
+def cal():
+    should_continue = True
+    num1 = float(input("What's the first number?:"))
+    for symbol in operations:
+        print(symbol)
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above:")
+        cal_func = operations[operation_symbol]
+        num2 = float(input("What's the next number?:"))
+        answer = cal_func(num1,num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            cal()
+
+cal()
