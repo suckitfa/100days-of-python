427f3e3b61d0391665f8b729529307e25266ad96
GuangyuanTang <254202042@qq.com>
2024-01-14 10:49:08 +0800

    fix: change the end num of range

--- day-13/main.py
+++ day-13/main.py
@@ -2,7 +2,8 @@
 
 # # Describe Problem
 def my_function():
-  for i in range(1, 20):
+#   omit the 21, so we should use range(1,21)
+  for i in range(1, 21):
     if i == 20:
       print("You got it")
 my_function()
