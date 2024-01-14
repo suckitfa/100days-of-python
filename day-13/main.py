3843d047ee339083689432d032af87f34bcb4d63
GuangyuanTang <254202042@qq.com>
2024-01-14 10:50:05 +0800

    fix: range out of index

--- day-13/main.py
+++ day-13/main.py
@@ -6,13 +6,13 @@ def my_function():
   for i in range(1, 21):
     if i == 20:
       print("You got it")
-my_function()
+# my_function()
 
 # # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 6)
+print(dice_imgs[dice_num-1])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
